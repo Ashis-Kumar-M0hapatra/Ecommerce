9445fe7d56bce027562a9faf8ad6ff0a2f0c2ddb
Ashis Kumar Mohapatra <akmo.linkan@gmail.com>
2025-04-26 13:15:06 +0530

    cart-page design

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
 import Home from "./pages/Home"
+import Cart from "./pages/Cart"
 function App() {
  
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/cart" element={<Cart />}></Route>
       </Routes>
       <Footer />
       </BrowserRouter>
---
+++ src/pages/Cart.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import EmptyCart from '../assets/Images/abandoned-cart.png'
+import { Link } from 'react-router-dom'
+import Home from './Home'
+const Cart = () => {
+  return (
+    <div className='flex flex-col items-center h-screen'>
+       <img src={EmptyCart} alt="" />
+         <p className='text-2xl text-center font-bold text-red-900 mt-4'>
+            Looks like you've not added anything! Don't make the cart sad! <br /> Go and explore our top excusive offers only for you
+            </p>
+            <button className='bg-red-900 text-white font-bold text-lg px-4 py-2 rounded-lg mt-4 hover:bg-red-800 active:translate-y-1 transform transition-transform duration-300 hover:scale-105'>
+                <Link to="/">
+                Hurry up! Shop Now
+                </Link>
+            </button>
+    </div>
+  )
+}
+
+export default Cart
